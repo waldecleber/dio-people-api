b6c9f4325c3bf4f6f56e37347ca9775984510e17
Waldecleber Gonçalves <walde@ni-tqi002990.tqi.com.br>
2021-08-31 17:03:48 -0300

    Create methods update and delete

--- src/main/java/br/com/waldecleber/people/controller/PersonController.java
+++ src/main/java/br/com/waldecleber/people/controller/PersonController.java
@@ -3,11 +3,13 @@ package br.com.waldecleber.people.controller;
 import br.com.waldecleber.people.dto.PersonDTO;
 import br.com.waldecleber.people.dto.response.MessageResponseDTO;
 import br.com.waldecleber.people.entity.Person;
+import br.com.waldecleber.people.exception.PersonNotFoundException;
 import br.com.waldecleber.people.service.PersonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -23,6 +25,27 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO person) {
         return personService.createPerson(person);
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll() {
+        return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.deleteById(id);
     }
 
 }
--- src/main/java/br/com/waldecleber/people/service/PersonService.java
+++ src/main/java/br/com/waldecleber/people/service/PersonService.java
@@ -3,11 +3,19 @@ package br.com.waldecleber.people.service;
 import br.com.waldecleber.people.dto.PersonDTO;
 import br.com.waldecleber.people.dto.response.MessageResponseDTO;
 import br.com.waldecleber.people.entity.Person;
+import br.com.waldecleber.people.exception.PersonNotFoundException;
 import br.com.waldecleber.people.repository.PersonRepository;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
@@ -30,6 +38,43 @@ public class PersonService {
         LOGGER.info(String.format("Created person with success", person.getId()));
         return MessageResponseDTO.builder()
                 .message("Created person with success")
+                .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(p -> mapper.map(p, PersonDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = verifyIfPersonExists(id);
+
+        return mapper.map(person, PersonDTO.class);
+    }
+
+    private Person verifyIfPersonExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id)
+                .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+
+    public void deleteById(Long id) throws PersonNotFoundException {
+        verifyIfPersonExists(id);
+        personRepository.deleteById(id);
+        LOGGER.info(String.format("Deleted person with success", id));
+    }
+
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfPersonExists(id);
+
+        Person person = mapper.map(personDTO, Person.class);
+        personRepository.save(person);
+        LOGGER.info(String.format("Updated person with success", person.getId()));
+
+        return MessageResponseDTO.builder()
+                .message("Updated person with success")
                 .build();
     }
 }
